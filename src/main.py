8d12c3e047d014cc9c80c4bfc56e2245bf9f2b32
Alexei Parphyonov <alexei.parphyonov@gmail.com>
2025-11-28 16:44:46 +0300

    refactored code into functions

--- src/main.py
+++ src/main.py
@@ -1,26 +1,50 @@
 import pygame as pg
-from window import display_size, display_width, display_height
-
 
 pg.init()
 
+# functions
+def player_update():
+    global player_x
+    player_x += player_dx
+
+def model_update():
+    player_update()
+
+def display_redraw():
+    display.fill('black', (0, 0, display_width, display_height))
+    display.blit(player_img, (player_x, player_y))
+    pg.display.update()
+
+def event_player(event):
+    pass
+
+def event_close_application(event):
+    close = False
+    if event.type == pg.QUIT:
+        close = True
+    return close
+
+def event_process():
+    """Processes keyboard & mouse events, returns False if the app is to be closed"""
+    for event in pg.event.get():
+        event_player(event)
+        if event_close_application(event):
+            return False
+    return True
 
 # load images
 icon_img = pg.image.load('src/img/ufo.png')
 player_img = pg.image.load('src/img/player.png')
-
 
 # window dimensions
 display_width = 800
 display_height = 600
 display_size = (display_width, display_height)
 
-
 # starting a window
 display = pg.display.set_mode(display_size)
 pg.display.set_caption('Space Invaders')
 pg.display.set_icon(icon_img)
-
 
 # player's pos
 player_width = player_img.get_width()
@@ -31,19 +55,13 @@ player_y = display_height - player_height - player_gap
 player_speed = 1
 player_dx = player_speed
 
-
 # running flag
 running = True
 while running:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
+    running = event_process()
 
     # objects movements
     player_x += player_dx
 
     # drawing inside the window
-    display.fill('black', (0, 0, display_width, display_height))
-    display.blit(player_img, (player_x, player_y))
-    pg.display.update()
-
+    display_redraw()
