2ed90183aaa1d40c117f0f1305552724a38c6d1e
Alexei Parphyonov <alexei.parphyonov@gmail.com>
2025-11-28 16:36:30 +0300

    finilized the movements, refactored code

--- src/main.py
+++ src/main.py
@@ -1,35 +1,49 @@
 import pygame as pg
 from window import display_size, display_width, display_height
 
+
 pg.init()
 
 
-# images section
+# load images
 icon_img = pg.image.load('src/img/ufo.png')
-pg.display.set_icon(icon_img)
-
 player_img = pg.image.load('src/img/player.png')
-player_width = player_img.get_width()
-player_height = player_img.get_height()
 
 
-# starting a player
-player_gap = 10
-player_x = display_width // 2 - player_width // 2
-player_y = display_height - player_height - player_gap
+# window dimensions
+display_width = 800
+display_height = 600
+display_size = (display_width, display_height)
 
 
 # starting a window
 display = pg.display.set_mode(display_size)
+pg.display.set_caption('Space Invaders')
+pg.display.set_icon(icon_img)
+
+
+# player's pos
+player_width = player_img.get_width()
+player_height = player_img.get_height()
+player_gap = 10
+player_x = display_width // 2 - player_width // 2
+player_y = display_height - player_height - player_gap
+player_speed = 1
+player_dx = player_speed
+
 
 # running flag
 running = True
-
-
 while running:
-    display.blit(player_img, (player_x, player_y))
-    pg.display.update()
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
+
+    # objects movements
+    player_x += player_dx
+
+    # drawing inside the window
+    display.fill('black', (0, 0, display_width, display_height))
+    display.blit(player_img, (player_x, player_y))
+    pg.display.update()
 
--- src/window.py
+++
@@ -1,4 +0,0 @@
-# window dimensions
-display_width = 800
-display_height = 600
-display_size = (display_width, display_height)
